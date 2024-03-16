e34ca9e8ab80927baad3c42eb1604f666476f85a
Krishna Rimal <baatomechanic@gmail.com>
2024-03-16 15:56:00 +0545

    Added app flavors

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'app.dart';
-import 'src/features/common/repositories/user_settings_repository.dart';
+import '../app.dart';
+import '../src/features/common/repositories/user_settings_repository.dart';
+import 'flavors.dart';
 
 final logger = BMLogger().logger;
 
-void main() async {
+Future<void> mainApp(Flavor flavor) async {
+  F.appFlavor = flavor;
   WidgetsFlutterBinding.ensureInitialized();
   final sharedPreferences = await SharedPreferences.getInstance();
   runApp(
